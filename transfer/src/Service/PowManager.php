2da4ac903c5f426d1d6f6f38502cae9804676884
ety001 <ety001@domyself.me>
2018-05-19 00:55:03 +0800

    fix pow

--- transfer/src/Service/PowManager.php
+++ transfer/src/Service/PowManager.php
@@ -5,19 +5,23 @@ namespace App\Service;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Users;
+use App\Service\Discord;
 
 class PowManager 
 {
     private $logger;
     private $em;
+    private $discord;
 
     public function __construct(
                         LoggerInterface $logger,
-                        EntityManagerInterface $em
+                        EntityManagerInterface $em,
+                        Discord $discord
                     )
     {
         $this->logger = $logger;
         $this->em = $em;
+        $this->discord = $discord;
     }
 
     public function powHandle($data)
@@ -27,17 +31,17 @@ class PowManager
             $username = $operation[1]['worker_account'];
             $json_metadata = [];
             $user = $this->em->getRepository(Users::class)->findOneBy(['username'=>$username]);
-            if ($user)
-                throw new \Exception('pow user exist');
-            $user = new Users();
-            $user->setUsername($username);
-            $user->setJsonMetadata($json_metadata);
-            $user->setIsPow(true);
-            $this->em->persist($user);
-            $this->em->flush();
-            $msg = 'pow user create success: '.json_encode($data);
-            $this->logger->info($msg);
-            echo $msg."\n";
+            if (!$user) {
+                $user = new Users();
+                $user->setUsername($username);
+                $user->setJsonMetadata($json_metadata);
+                $user->setIsPow(true);
+                $this->em->persist($user);
+                $this->em->flush();
+                $msg = 'pow_user_create_success: '.json_encode($data);
+                $this->logger->info($msg);
+                echo $msg."\n";
+            }
         } catch(\Exception $e) {
             $msg = 'pow_user_create_error: '.$e->getMessage().", ".json_encode($data);
             $this->logger->error($msg);
@@ -53,17 +57,17 @@ class PowManager
             $username = $operation[1]['work'][1]['input']['worker_account'];
             $json_metadata = [];
             $user = $this->em->getRepository(Users::class)->findOneBy(['username'=>$username]);
-            if ($user)
-                throw new \Exception('pow2 user exist');
-            $user = new Users();
-            $user->setUsername($username);
-            $user->setJsonMetadata($json_metadata);
-            $user->setIsPow(true);
-            $this->em->persist($user);
-            $this->em->flush();
-            $msg = 'pow2_user_create_success: '.json_encode($data);
-            $this->logger->info($msg);
-            echo $msg."\n";
+            if (!$user) {
+                $user = new Users();
+                $user->setUsername($username);
+                $user->setJsonMetadata($json_metadata);
+                $user->setIsPow(true);
+                $this->em->persist($user);
+                $this->em->flush();
+                $msg = 'pow2_user_create_success: '.json_encode($data);
+                $this->logger->info($msg);
+                echo $msg."\n";
+            }
         } catch(\Exception $e) {
             $msg = 'pow2_user_create_error: '.$e->getMessage().", ".json_encode($data);
             $this->logger->error($msg);
