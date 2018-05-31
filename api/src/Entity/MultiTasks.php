05d938d62e501adc73cbb969b08f00419509e343
ety001 <ety001@domyself.me>
2018-06-01 04:09:42 +0800

    change multi_tasks primary_key type from int to bigint

--- api/src/Entity/MultiTasks.php
+++ api/src/Entity/MultiTasks.php
@@ -12,7 +12,7 @@ class MultiTasks
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer", options={"unsigned"=true})
+     * @ORM\Column(type="bigint", options={"unsigned"=true})
      */
     private $id;
 
