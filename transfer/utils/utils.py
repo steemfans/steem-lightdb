40a6547e12510afac10dfa4ef9ad1b26271ac2c8
ety001 <ety001@domyself.me>
2018-06-01 11:14:11 +0800

    fix env

--- transfer/utils/utils.py
+++ transfer/utils/utils.py
@@ -6,14 +6,14 @@ import json, os, sys, time
 def get_config():
     c = {}
     env_dist = os.environ
-    c['worker'] = env_dist.get('WORKER')
-    if c['worker'] == None:
+    c['worker'] = int(env_dist.get('WORKER'))
+    if c['worker'] == 0:
         c['worker'] = 5
     if c['worker'] > 50:
         print('threads are too many')
         sys.exit()
-    c['block_step'] = env_dist.get('BLOCK_STEP')
-    if c['block_step'] == None:
+    c['block_step'] = int(env_dist.get('BLOCK_STEP'))
+    if c['block_step'] == 0:
         c['block_step'] = 100
     try:
         c['steemdb_config'] = json.loads(env_dist.get('STEEMDB_CONFIG'))
