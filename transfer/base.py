e92b7f38547bf0eccdd0a4b281bab3703ec7ee73
ety001 <ety001@domyself.me>
2018-06-17 16:21:23 +0800

    finish rewrite base layer

--- transfer/base.py
+++ transfer/base.py
@@ -11,29 +11,30 @@ import queue
 import threading
 import pymysql
 
+check_point_sum = 0
 config = utils.get_config()
 base_step = config['base_step']
 base_thread_count = config['base_thread_count']
 
 steemd_nodes = [
-    'https://rpc.buildteam.io',
-    'https://steemd.privex.io',
     'https://api.steemit.com',
-    'https://rpc.steemviz.com',
+    'https://rpc.buildteam.io',
+    #'https://steemd.privex.io',
+    #'https://rpc.steemviz.com',
 ]
 s = Steemd(nodes=steemd_nodes, maxsize=base_step*base_thread_count)
 b = Blockchain(s)
 
 def processor(task_queue):
-    global s, b
+    global s, b, check_point_sum
     while task_queue.qsize():
         tasks = task_queue.get()
         print('get_blocks_start', tasks)
         task_start_time = time.time()
         try:
-            block_infos = s.get_blocks(tasks)
-        except:
-            print('get_data_from_chain_failed:', tasks)
+            block_infos = s.get_blocks(tasks['content'])
+        except Exception as e:
+            print('get_data_from_chain_failed:', tasks, e)
             return
     
         print('got_data', tasks)
@@ -58,6 +59,12 @@ def processor(task_queue):
         r = saveToMysql(insert_data)
         if r == True:
             task_queue.task_done()
+            if tasks['task_type'] == 'lost_block':
+                check_point_sum += 1
+                print(check_point_sum, tasks['total_slice'])
+                if check_point_sum == tasks['total_slice']:
+                    # lost blocks have been filled.
+                    updateCheckPoint(tasks['next_check_point'])
             task_end_time = time.time()
             print('get_blocks_end', tasks, task_end_time - task_start_time)
         else:
@@ -96,11 +103,38 @@ def saveToMysql(insert_data):
             return False
     db.close()
 
+def updateCheckPoint(check_point):
+    config = utils.get_config()
+    db_c = config['steem_config']
+    db = pymysql.connect(
+        host=db_c['host'],
+        port=db_c['port'],
+        user=db_c['user'],
+        password=db_c['pass'],
+        charset='utf8mb4',
+        db=db_c['db'],
+        autocommit=False)
+
+    with db.cursor() as cursor:
+        try:
+            sql = '''update config
+                set val = %s
+                where param = "check_point"'''
+            cursor.execute(sql, (check_point, ))
+            db.commit()
+            return True
+        except Exception as e:
+            db.rollback()
+            print('[warning]insert block cache error', e, sql, insert_data)
+            return False
+    db.close()
+
 def quit(signum, frame):
     print('quit')
     sys.exit()
 
 def mainMultiProcess():
+    global check_point_sum
     try:
         config = utils.get_config()
         base_sleep = config['base_sleep']
@@ -112,12 +146,11 @@ def mainMultiProcess():
 
         while True:
             all_tasks = BaseTasks.get()
+            # print('all_tasks', all_tasks)
             if all_tasks == []:
                 print('no_tasks')
                 continue
 
-            #task_length = len(all_tasks)
-            #slice_tasks = [ all_tasks[i:i+base_slice_step] for i in range(0, task_length, base_slice_step) ]
             task_queue = queue.Queue()
             for tmp_tasks in all_tasks:
                 task_queue.put(tmp_tasks)
@@ -132,6 +165,7 @@ def mainMultiProcess():
                 t.start()
             task_queue.join() # suspend before all tasks finished
 
+            check_point_sum = 0
             time.sleep(base_sleep)
     except Exception as e:
         utils.PrintException()
--- transfer/utils/BaseTasks.py
+++ transfer/utils/BaseTasks.py
@@ -6,6 +6,8 @@ import json, os, sys, time
 import utils.utils as utils
 from steem.blockchain import Blockchain
 from steem.steemd import Steemd
+import queue
+import threading
 
 def get():
     # get db config
@@ -22,7 +24,7 @@ def get():
         autocommit = True)
 
     # check lost block first
-    checkLostBlockCache(db, config)
+    result = checkLostBlockCache(db, config)
 
     # get synced head_block_num
     sql = 'select block_num from block_cache order by block_num desc limit 1'
@@ -42,10 +44,10 @@ def get():
 
     # get lastest block_num
     steemd_nodes = [
-        'https://rpc.buildteam.io',
-        'https://steemd.privex.io',
         'https://api.steemit.com',
-        'https://rpc.steemviz.com',
+        'https://rpc.buildteam.io',
+        #'https://steemd.privex.io',
+        #'https://rpc.steemviz.com',
     ]
     s = Steemd(nodes=steemd_nodes)
     b = Blockchain(s)
@@ -60,99 +62,83 @@ def get():
     start_num = curr_cache_head + 1
     end_num = head_block_number
 
-    result = []
     i = start_num
     while i <= end_num:
         tmp_end_num = i + base_step
         if tmp_end_num >= end_num:
             tmp_end_num = end_num
-        result.append(range(i, tmp_end_num + 1))
+        result.append({"task_type": "new_block", "content": range(i, tmp_end_num + 1)})
         i = tmp_end_num + 1
 
     return result
 
+def updateCheckPoint(db, v):
+    print('update_check_point_in_base_tasks', v)
+    if v == 1:
+        sql = 'insert into config (param, val) values ("check_point", %s)'
+    else:
+        sql = 'update config set val = %s where param = "check_point"'
+    with db.cursor() as cur:
+        cur.execute(sql, (v,))
+
 def checkLostBlockCache(db, config):
     print('check_lost_block_cache')
     # get synced head_block_num
-    sql = 'select max(block_num) as max_num, min(block_num) as min_num from block_cache'
+    sql = 'select block_num from block_cache order by block_num desc limit 1'
     with db.cursor() as cur:
         cur.execute(sql)
         res = cur.fetchone()
-        if res['min_num'] == None or res['max_num'] == None:
-            print('records_not_exist')
-            return
-    start_num = int(res['min_num'])
-    end_num = int(res['max_num'])
+        if res == None:
+            print('block_cache_not_exist')
+            return []
+        else:
+            max_num = res['block_num']
+
+    sql = 'select val from config where param = "check_point"'
+    with db.cursor() as cur:
+        cur.execute(sql)
+        res = cur.fetchone()
+        if res == None:
+            print('check_point_not_exist')
+            check_point = 1
+            updateCheckPoint(db, 1)
+        else:
+            print('to_get_check_point', res)
+            check_point = int(res['val'])
 
     sql = 'select block_num from block_cache where block_num >= %s and block_num <= %s order by block_num asc'
     with db.cursor() as cur:
-        cur.execute(sql, (start_num, end_num))
+        cur.execute(sql, (check_point, max_num))
         res = cur.fetchall()
-        print('abc', res)
-        if res == ():
-            print('records_not_exist2')
-            return
-    exists_num = []
+        if res == []:
+            print('block_cache_not_exist_2')
+            return []
     records_count = len(res)
     
-    if (end_num - start_num + 1) <= records_count:
+    if (max_num - check_point + 1) <= records_count:
         print('no_lost_block_cache')
-        return
+        return []
 
     print('detect_lost_block_cache')
-    all_list = range(start_num, end_num + 1)
+    all_list = range(check_point, max_num + 1)
     exists_num = []
     for r in res:
         exists_num.append(r['block_num'])
-    lost_blocks = list(set(all_list) - set(exists))
-
-    # grouped
-    #length = len(lost_blocks)
-    #if length == 0:
-    #    return
-    #base_step = config['base_step']
-    #r = [lost[i:i+base_step] for i in range(0, length, base_step)]
-    try:
-        block_infos = s.get_blocks(lost_blocks)
-    except Exception as e:
-        print('got_lost_block_error', e)
-        return
-
-    insert_data = []
-    for block in block_infos:
-        block_num = block['block_num']
-        previous = block['previous']
-        block_id = block['block_id']
-        timestamp = utils.strtotime(block['timestamp'])
-        del block['previous']
-        del block['block_id']
-        del block['timestamp']
-        del block['block_num']
-        tmp_block_info = json.dumps(block)
-        insert_data.append((
-            block_num,
-            previous,
-            block_id,
-            tmp_block_info,
-            timestamp))
-
-    with db.cursor() as cursor:
-        try:
-            sql = '''
-                insert into `block_cache` (
-                `block_num`,
-                `previous`,
-                `block_id`,
-                `block_info`,
-                `timestamp`
-                ) values (%s, %s, %s, %s, %s)'''
-            cursor.executemany(sql, insert_data)
-            conn.commit()
-        except Exception as e:
-            conn.rollback()
-            print('[warning]insert lost block cache error', e, sql, insert_data)
-            return
-    db.close()
+    lost_blocks = list(set(all_list) - set(exists_num))
+    
+    # get lost blocks
+    base_sleep = config['base_sleep']
+    base_slice_step = config['base_slice_step']
+    base_thread_count = config['base_thread_count']
+    lost_blocks_length = len(lost_blocks)
+    if lost_blocks_length <= 0:
+        return []
+    lost_blocks_slice = [ lost_blocks[i:i+base_slice_step] for i in range(0, lost_blocks_length, base_slice_step) ]
+    result = []
+    total_slice = len(lost_blocks_slice)
+    for lb in lost_blocks_slice:
+        result.append({"task_type":"lost_block", "content": lb, "total_slice": total_slice, "next_check_point": max_num + 1})
+    return result
 
 if __name__ == '__main__':
     data = get()
